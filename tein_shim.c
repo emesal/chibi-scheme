a927303e9e05af2b24e83e88df37c4a988628c4f
fey <fey@emesal.net>
2026-03-12 17:56:48 +0000

    fix: remove duplicate cell_n declaration

--- tein_shim.c
+++ tein_shim.c
@@ -702,10 +702,7 @@ sexp tein_env_bindings_list(sexp ctx, sexp prefix) {
          * the next-cell pointer is sexp_env_next_cell (pair source field),
          * NOT cdr. iterate with sexp_env_next_cell. */
         cell = sexp_env_bindings(env);
-        int cell_n = 0;
-        int cell_n = 0;
         while (sexp_pairp(cell)) {
-            cell_n++;
             /* recover name/value from the rooted cell after every alloc */
             sexp name  = sexp_car(cell);
             sexp value = sexp_cdr(cell);
