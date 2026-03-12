9e25a9afd2450685a139d952bcb6ad202b75a0b7
fey <fey@emesal.net>
2026-03-12 17:22:02 +0000

    feat: add procedure-arity, env-bindings, imported-modules to (tein introspect)

--- lib/tein/introspect.scm
+++ lib/tein/introspect.scm
@@ -6,3 +6,12 @@
 (define (available-modules) (tein-available-modules-internal))
 
 (define (module-exports mod-path) (tein-module-exports-internal mod-path))
+
+(define (procedure-arity proc) (tein-procedure-arity-internal proc))
+
+(define (env-bindings . args)
+  (if (null? args)
+      (tein-env-bindings-internal)
+      (tein-env-bindings-internal (car args))))
+
+(define (imported-modules) (tein-imported-modules-internal))
--- lib/tein/introspect.sld
+++ lib/tein/introspect.sld
@@ -1,5 +1,8 @@
 (define-library (tein introspect)
   (import (scheme base) (scheme write) (scheme eval) (chibi))
   (export available-modules
-          module-exports)
+          module-exports
+          procedure-arity
+          env-bindings
+          imported-modules)
   (include "introspect.scm"))
